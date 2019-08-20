9ad9ea679b4c47c62c867b5044a26a9481ed3057
Matthew Sawatzky <34319870+msawatzky75@users.noreply.github.com>
2019-08-20 14:52:45 -0500

    Completed addition and subtraction

--- src/StrInt.ts
+++ src/StrInt.ts
@@ -1,8 +1,10 @@
-export default class StrInt {
+type Target = StrInt | string | number
+export default class StrInt implements IComparable<Target> {
 	value: string;
 	positive: boolean;
 
-	constructor(num: any) {
+	constructor(num?: any) {
+		this.value = "0";
 		this.positive = true;
 		if (typeof num === 'string') {
 			if (/^\d+$/.test(num as string)) {
@@ -12,12 +14,12 @@ export default class StrInt {
 			if (num < 0) {
 				this.positive = false;
 			}
-			this.value = num.toString();
+			this.value = Math.abs(num).toString();
 		} else if (num instanceof StrInt) {
 			this.value = num.value;
 			this.positive = num.positive;
 		} else if (num) {
-			throw new Error(`Invalid type ${typeof num}`);
+			throw new Error('Invalid type.');
 		}
 	}
 
@@ -40,65 +42,140 @@ export default class StrInt {
 		}
 	}
 
-	negate(): StrInt {
-		this.positive = !this.positive;
+	negate(negative?: boolean): StrInt {
+		if (negative !== undefined) {
+			this.positive = !negative;
+		} else {
+			this.positive = !this.positive;
+		}
 		return this;
 	}
 
-	add(target: StrInt | string | number): StrInt {
+	abs(): StrInt {
+		return new StrInt(this.value);
+	}
+
+	add(target: Target): StrInt {
 		const other = new StrInt(target);
 
 		// skip zeros, they dont change the value, and cause issues.
 		if (other.value === '0') {
 			return this;
 		}
-		if (other.positive != this.positive) {
-			return this.subtract(other);
+
+		if (other.positive !== this.positive) {
+			return this.subtract(other.negate());
 		}
 
 		for (let i = 0; i < other.value.length; i++) {
-			let result = Number(other.getDigit(i)) + Number(this.getDigit(i));
-			if (result.toString() === this.getDigit(i)) {
+			let result = Number(this.getDigit(i)) + Number(other.getDigit(i));
+			if (result.toString() === this.getDigit(i))
 				continue;
-			}
 
 			this.setDigit(i, result % 10);
 
 			// if there is carryover, add it.
-			if (result > 9) {
-				this.add(result - (result % 10));
-			}
+			if (result > 9)
+				this.add(new StrInt(result - (result % 10)).negate(!other.positive));
 		}
 
 		return this;
 	}
 
-	subtract(target: StrInt | string | number): StrInt {
+	subtract(target: Target): StrInt {
 		const other = new StrInt(target);
 
 		if (other.value === '0') {
 			return this;
 		}
-		if (other.positive != this.positive) {
-			return this.add(other);
+
+		if (other.positive !== this.positive) {
+			return this.add(other.negate());
 		}
 
 		for (let i = 0; i < other.value.length; i++) {
 			let result = Number(this.getDigit(i)) - Number(other.getDigit(i));
-			if (result.toString() === this.getDigit(i)) {
+			if (result.toString() === this.getDigit(i))
 				continue;
-			}
 
-			this.setDigit(i, result < 0 ? 10 + result : result);
-			if (result < 0) {
-				this.subtract(Math.abs(result * Math.pow(10, other.value.length - i)));
+			if (this.abs().lt(other.abs())) {
+				this.negate();
+				let temp = this.value;
+				this.value = other.value;
+				other.value = temp;
+				this.setDigit(i, Math.abs(result));
+			} else {
+				this.setDigit(i, result < 0 ? 10 + result : result);
+				if (result < 0) {
+					this.subtract(new StrInt(Math.pow(10, other.value.length - i)).negate(!other.positive));
+				}
 			}
 		}
 
 		return this;
 	}
 
-	toString(): string {
-		return this.value;
+	compareTo(target: Target): Comparison {
+		const other = new StrInt(target);
+		if (this.positive && !other.positive) {
+			return Comparison.greater;
+		} else if (other.positive && !this.positive) {
+			return Comparison.lesser;
+		}
+		// we can now assume they have the same sign
+		// if both are positive
+		if (this.positive && (other.positive == this.positive)) {
+			if (this.value.length > other.value.length) {
+				return Comparison.greater;
+			} else if (this.value.length < other.value.length) {
+				return Comparison.lesser;
+			} else { // both are same length
+				return compareValue(this, other);
+			}
+		} else { // Both are negative
+			if (this.value.length < other.value.length) {
+				return Comparison.greater;
+			} else if (this.value.length > other.value.length) {
+				return Comparison.lesser;
+			} else { // both are same length
+				// this will be inverted, we need this because both are negative
+				return compareValue(other, this);
+			}
+		}
+		function compareValue(value1: StrInt, value2: StrInt): Comparison {
+			for (let i = value1.value.length - 1; i >= 0; i--) {
+				if (Number(value1.getDigit(i)) > Number(value2.getDigit(i))) {
+					return Comparison.greater;
+				} else if (Number(value1.getDigit(i)) < Number(value2.getDigit(i))) {
+					return Comparison.lesser;
+				}
+			}
+			return Comparison.equal;
+		}
 	}
+
+	gt(target: Target): boolean {
+		return this.compareTo(target) === Comparison.greater;
+	}
+
+	lt(target: Target): boolean {
+		return this.compareTo(target) === Comparison.lesser;
+	}
+
+	equals(target: Target): boolean {
+		return this.compareTo(target) === Comparison.equal;
+	}
+
+	toString(): string {
+		return `${this.positive ? '' : '-'}${this.value}`;
+	}
+}
+
+export interface IComparable<T = any> {
+	compareTo: (other: T) => Comparison
+}
+export enum Comparison {
+	lesser = -1,
+	equal = 0,
+	greater = 1,
 }
--- test/StrInt.test.ts
+++ test/StrInt.test.ts
@@ -1,26 +1,114 @@
-import StrInt from '../src/StrInt';
+import StrInt, {Comparison} from '../src/StrInt';
+
+test('constructor string', () => {
+	expect(new StrInt("7").toString()).toBe("7");
+});
+test('constructor number', () => {
+	expect(new StrInt(7).toString()).toBe("7");
+});
+test('constructor negative number', () => {
+	expect(new StrInt(-7).toString()).toBe("-7");
+});
+test('constructor invalid type', () => {
+	expect(() => new StrInt([7])).toThrow()
+});
+
+test('negate default positive', () => {
+	expect(new StrInt(5).negate().toString()).toBe("-5")
+});
+test('negate default negative', () => {
+	expect(new StrInt(-5).negate().toString()).toBe("5")
+});
+test('negate provided positive', () => {
+	expect(new StrInt(5).negate(true).toString()).toBe("-5")
+});
+test('negate provided negative', () => {
+	expect(new StrInt(-5).negate(false).toString()).toBe("5")
+});
 
 test('simple sum', () => {
 	expect(new StrInt(4).add(4).toString()).toBe("8");
 });
-
 test('multi-digit sum', () => {
 	expect(new StrInt(123).add(123).toString()).toBe("246");
 });
-
 test('overflow sum', () => {
-	expect(new StrInt(9).add(5).toString()).toBe("14");
+	expect(new StrInt(48).add(3).toString()).toBe("51");
 });
-
+test('negative sum', () => {
+	expect(new StrInt(65).negate().add(5).toString()).toBe("-60");
+});
+test('add zero', () => { // coverage test
+	expect(new StrInt(845).add(0).toString()).toBe("845");
+});
+test('sum past zero', () => {
+	expect(new StrInt(-4).add(5).toString()).toBe("1");
+});
+test('sum multi-digit past zero', () => {
+	expect(new StrInt(-42).add(53).toString()).toBe("11");
+});
 
 test('simple subtract', () => {
 	expect(new StrInt(9).subtract(3).toString()).toBe("6");
 });
-
 test('multi-digit subtract', () => {
 	expect(new StrInt(123).subtract(23).toString()).toBe("100");
 });
-
 test('overflow subtract', () => {
 	expect(new StrInt(21).subtract(3).toString()).toBe("18");
+});
+test('negative subtract', () => {
+	expect(new StrInt(48).negate().subtract(3).toString()).toBe("-51");
+});
+test('subtract zero', () => { // coverage test
+	expect(new StrInt(845).subtract(0).toString()).toBe("845");
+});
+test('subtract past zero', () => {
+	expect(new StrInt(4).subtract(5).toString()).toBe("-1");
+});
+test('subtract multi-digit past zero', () => {
+	expect(new StrInt(40).subtract(53).toString()).toBe("-13");
+});
+
+test('compare larger', () => {
+	expect(new StrInt(9).compareTo(1)).toBe(Comparison.greater);
+});
+test('compare smaller', () => {
+	expect(new StrInt(3).compareTo(7)).toBe(Comparison.lesser);
+});
+test('compare equal', () => {
+	expect(new StrInt(81).compareTo(81)).toBe(Comparison.equal);
+});
+test('compare larger helper', () => {
+	expect(new StrInt(9).gt(1)).toBe(true);
+});
+test('compare smaller helper', () => {
+	expect(new StrInt(3).lt(7)).toBe(true);
+});
+test('compare equal helper', () => {
+	expect(new StrInt(81).equals(81)).toBe(true);
+});
+test('compare positive to negative', () => {
+	expect(new StrInt(81).compareTo(-81)).toBe(Comparison.greater);
+});
+test('compare negative to positive', () => {
+	expect(new StrInt(-81).compareTo(81)).toBe(Comparison.lesser);
+});
+test('compare bigger positive to smaller positive', () => {
+	expect(new StrInt(125).compareTo(76)).toBe(Comparison.greater);
+});
+test('compare smaller positive to bigger positive', () => {
+	expect(new StrInt(52).compareTo(706)).toBe(Comparison.lesser);
+});
+test('compare equal length positive', () => {
+	expect(new StrInt(502).compareTo(706)).toBe(Comparison.lesser);
+});
+test('compare bigger negative to smaller negative', () => {
+	expect(new StrInt(-125).compareTo(-76)).toBe(Comparison.lesser);
+});
+test('compare smaller negative to bigger negative', () => {
+	expect(new StrInt(-52).compareTo(-706)).toBe(Comparison.greater);
+});
+test('compare equal length negative', () => {
+	expect(new StrInt(-502).compareTo(-706)).toBe(Comparison.greater);
 });
